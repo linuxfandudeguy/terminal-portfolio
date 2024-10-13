0bdd5245e736a75371b704cd98e488f760c04b76
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2024-10-13 09:04:26 -0400

    Update About.tsx

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -8,15 +8,14 @@ const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
       <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
+        Hi, my name is <HighlightSpan>lelbois</HighlightSpan>!
       </p>
       <p>
-        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
-        Myanmar.
+        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in the US.
       </p>
       <p>
-        I am passionate about writing codes and <br />
-        developing web applications to solve real-life challenges.
+        I love writing code and <br />
+        developing web applications.
       </p>
     </AboutWrapper>
   );
