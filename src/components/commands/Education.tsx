9609e4fb6d891764ae531a6b7addc2e869fe4088
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2024-10-13 08:35:33 -0400

    Update Education.tsx

--- src/components/commands/Education.tsx
+++ src/components/commands/Education.tsx
@@ -4,7 +4,7 @@ import { Wrapper } from "../styles/Output.styled";
 const Education: React.FC = () => {
   return (
     <Wrapper data-testid="education">
-      <EduIntro>Here is my education background!</EduIntro>
+      <EduIntro> </EduIntro>
       {eduBg.map(({ title, desc }) => (
         <EduList key={title}>
           <div className="title">{title}</div>
@@ -17,16 +17,16 @@ const Education: React.FC = () => {
 
 const eduBg = [
   {
-    title: "B.Sc (Hons) in Computing",
-    desc: "Edinburgh Napier University | 2018 ~ 2019",
+    title: " ",
+    desc: "",
   },
   {
-    title: "HND in Computing & System Development",
-    desc: "Info Myanmar University | 2016 - 2018",
+    title: " ",
+    desc: " ",
   },
   {
-    title: "IELTS 6.5",
-    desc: "British Council Myanmar | 2017",
+    title: " ",
+    desc: " ",
   },
 ];
 
