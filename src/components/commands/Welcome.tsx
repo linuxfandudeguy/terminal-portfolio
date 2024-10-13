963e216b128aa361a0ff770460db4449abce238b
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2024-10-13 08:45:55 -0400

    Update Welcome.tsx

--- src/components/commands/Welcome.tsx
+++ src/components/commands/Welcome.tsx
@@ -15,35 +15,36 @@ const Welcome: React.FC = () => {
       <div className="info-section">
         <PreName>
           {`        
-    _____       __     _   __      _            
-   / ___/____ _/ /_   / | / /___ _(_)___  ____ _
-   \\__ \\/ __ \`/ __/  /  |/ / __ \`/ / __  / __ \`/
-  ___/ / /_/ / /_   / /|  / /_/ / / / / / /_/ / 
- /____/\\__,_/\\___/ /_/ |_/\\__,_/_/_/ /_/\\__, /  
-                                       /____/   
-          `}
+ _      _ _           _     
+| | ___| | |__   ___ (_)___ 
+| |/ _ \ | '_ \ / _ \| / __|
+| |  __/ | |_) | (_) | \__ \
+|_|\___|_|_.__/ \___/|_|___/
+`}
         </PreName>
         <PreWrapper>
           <PreNameMobile>
             {`
-    ____     __          
-   / __/__ _/ /_         
-  _\\ \\/ _ \`/ __/         
- /___/\\_,_/\\__/          
-    _  __     _          
-   / |/ /__ _(_)__  ___ _
-  /    / _ \`/ / _ \\/ _ \`/
- /_/|_/\\_,_/_/_//_/\\_, / 
-                  /___/  
- 
-          `}
+  _      _ _     
+| | ___| | |__  
+| |/ _ \ | '_ \ 
+| |  __/ | |_) |
+|_|\___|_|_.__/ 
+                
+       _     
+  ___ (_)___ 
+ / _ \| / __|
+| (_) | \__ \
+ \___/|_|___/          
+`}
           </PreNameMobile>
         </PreWrapper>
         <div>Welcome to my terminal portfolio. (Version 1.3.1)</div>
         <Seperator>----</Seperator>
         <div>
-          This project's source code can be found in this project's{" "}
-          <Link href="https://github.com/satnaing/terminal-portfolio">
+           This is a fork from a user by the name of satnaing.
+           This project's source code can be found in this project's{" "}
+          <Link href="https://github.com/linuxfandudeguy/terminal-portfolio">
             GitHub repo
           </Link>
           .
@@ -56,24 +57,19 @@ const Welcome: React.FC = () => {
       <div className="illu-section">
         <PreImg>
           {`
-                       ,##,,eew,
-                     ,##############C
-                  a###############@##
-                 7####^\`^"7W7^"@####
-                 @#@b\`         ^@#@^
-                  ##^,,,,   ,,,,^#^
-                 ,,@######"#######=
-                  .''555"\` '5555b|
-                  T"@  ,,,^,mg,@,*
-                     %p||\`~~'.#\`
-                      ^Wp  ,#T
-                     :b''@@b^}
-                  ,^     \` 'b 3-
-              .<\` 'p   ^v   #   b   *.
-            {      }   #"GpGb   [
-            C      3 * @#######Nl      \`
-           '            ^@##b     ($    !
-         `}
+                 
+   @@          @@   
+  @@@@@      @@@@@  
+ @@@@@@@@@@@@@@@@@@ 
+ @@@@@@@@@@@@@@@@@@ 
+@@@@@@@@@@@@@@@@@@@@
+@@@*   @@@@@@   +@@@
+@@@@@@*@@@@@@@@*#@@@
+@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@
+ @@@@@@@@@@@@@@@@@@ 
+    @@@@@@@@@@@@           
+`}
         </PreImg>
       </div>
     </HeroContainer>
